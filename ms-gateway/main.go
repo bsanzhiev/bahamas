009f29d51dbc9e74a3d62fd16a5c812aea795fa7
Baatar <baatar.s@techpartners.asia>
2024-05-06 18:10:16 +0800

    error import packages

--- ms-gateway/main.go
+++ ms-gateway/main.go
@@ -10,20 +10,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// RequestData Тип для запросов
-type RequestData struct {
-	Service string                 `json:"service"` // Имя сервиса
-	Action  string                 `json:"action"`  // Имя операции
-	Data    map[string]interface{} `json:"data"`    // Объект запроса
-}
-
-// ResponseData Тип для ответов
-type ResponseData struct {
-	Status  int         `json:"status"`  // Код ответа
-	Message string      `json:"message"` // Сообщение об ошибке или результате
-	Data    interface{} `json:"data"`    // Объект ответа
-}
-
 func main() {
 	// Создаем новый экземпляр Fiber
 	app := fiber.New(
---
+++ ms-gateway/types.go
@@ -0,0 +1,15 @@
+package ms_gateway
+
+// RequestData Тип для запросов
+type RequestData struct {
+	Service string                 `json:"service"` // Имя сервиса
+	Action  string                 `json:"action"`  // Имя операции
+	Data    map[string]interface{} `json:"data"`    // Объект запроса
+}
+
+// ResponseData Тип для ответов
+type ResponseData struct {
+	Status  int         `json:"status"`  // Код ответа
+	Message string      `json:"message"` // Сообщение об ошибке или результате
+	Data    interface{} `json:"data"`    // Объект ответа
+}
