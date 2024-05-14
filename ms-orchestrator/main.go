d08de34940669aa5f52cea82ce0c8d8a46bc5978
Baatar <baatar.s@techpartners.asia>
2024-05-14 18:02:34 +0800

    update ms orchestrator

--- ms-orchestrator/main.go
+++ ms-orchestrator/main.go
@@ -3,22 +3,46 @@ package main
 import (
 	"github.com/nats-io/nats.go"
 	"log"
+	"time"
 )
 
+// Account Creation
+// 1. Account Service receive action for create account
+// 2. Orchestrator
+
 func main() {
+	// Connect to NATS
 	nc, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error connecting NATS: %v", err)
 	}
 	defer nc.Close()
 
+	// Subscribe to start.saga to initiate saga
 	nc.Subscribe("start.saga", func(m *nats.Msg) {
 		log.Println("Saga started for: ", string(m.Data))
-		// Implement saga logic
-		err := nc.Publish("ms-users.create", m.Data)
+
+		// Step 1: Request a user ID from the users service
+		msg, err := nc.Request("ms-users.getID", m.Data, 10*time.Second)
 		if err != nil {
+			log.Printf("Error getting user ID: %v", err)
 			return
 		}
+		userID := string(msg.Data)
+		log.Printf("Received user ID: %v", userID)
+
+		// Step 2: Create a bank account
+		err = nc.Publish("ms-accounts.createAccount", []byte(userID))
+		if err != nil {
+			log.Printf("Error creating bank account: %v", err)
+			return
+		}
+		log.Printf("Bank account creation requested for user ID: %s", userID)
+
+		// Optionally, listen for a response from the account service
+		nc.Subscribe("ms-account.createAccount.response", func(m *nats.Msg) {
+			log.Println("Bank account creation: ", string(m.Data))
+		})
 	})
 	select {}
 }
